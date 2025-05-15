a3be1772c500bbe8dbf4cb219fbbe2646e22709f
TorresAnndy <andygonzales2003@gmail.com>
2025-05-15 10:35:55 -0500

    modified:   docker-compose.yml 	modified:   src/main/java/com/my_project/my_project/model/Book.java 	modified:   src/main/java/com/my_project/my_project/repository/BookRepository.java 	modified:   src/main/java/com/my_project/my_project/service/BookService.java

--- src/main/java/com/my_project/my_project/model/Book.java
+++ src/main/java/com/my_project/my_project/model/Book.java
@@ -2,15 +2,27 @@ package com.my_project.my_project.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="book")
 public class Book implements Serializable {
 
-    private String bookId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bookId;
     private String title;
     private String author;
     private Integer publishedYear;
--- src/main/java/com/my_project/my_project/service/BookService.java
+++ src/main/java/com/my_project/my_project/service/BookService.java
@@ -1,6 +1,5 @@
 package com.my_project.my_project.service;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,35 +10,46 @@ import com.my_project.my_project.repository.BookRepository;
 
 @Service
 public class BookService {
+	
+	@Autowired
+	BookRepository bookRepository;
+	
+	//obtener todos los libros
+	public Iterable<Book> getAllBooks(){
+		Iterable<Book> books= bookRepository.findAll();
+		return books;
+	}
+	
+	//crear
+	public Book createBook(Book book) {
+		Book bookCreated = bookRepository.save(book);
+		return bookCreated;
+	}
+	
+	//buscar por Title
+	public Optional<Book> findBookByTitle(String title){
+		return bookRepository.findBookByTitle(title);
+	}
+	//buscar por Id
+	public Optional<Book> findBookById(Long id){
+		return bookRepository.findById(id);
+	}
+	
+	//borrar por title
+	public Book deleteBookByTitle(String title){
+		Optional<Book> deleteBook= bookRepository.deleteBookByTitle(title);
+		return null;
+	}
+	
+	//acrtualizar
+	public Book updateBook(Book book) {
+		return bookRepository.save(book);
+	}
+	
+	//borrar
+	public void deleteBook(Book book) {
+		bookRepository.delete(book);
+	}
+	
 
-    @Autowired
-    BookRepository bookRepository;
-
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
-    }
-
-    public Book createBook(Book book) {
-        return bookRepository.save(book);
-    }
-
-    public Optional<Book> findBookByTitle(String title) {
-        return bookRepository.findBookByTitle(title);
-    }
-
-    public Optional<Book> findBookById(String id) {
-        return bookRepository.findById(id);
-    }
-
-    public Optional<Book> deleteBookByTitle(String title) {
-        return bookRepository.deleteBookByTitle(title);
-    }
-
-    public Book updateBook(Book book) {
-        return bookRepository.save(book);
-    }
-
-    public void deleteBook(Book book) {
-        bookRepository.delete(book);
-    }
 }
