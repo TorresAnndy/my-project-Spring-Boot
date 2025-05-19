67bde3bb3dc63e747a4ba1839654760fc50f23df
TorresAnndy <andygonzales2003@gmail.com>
2025-05-19 00:04:19 -0500

    modified:   src/main/java/com/my_project/my_project/controller/LibraryWebController.java

--- src/main/java/com/my_project/my_project/controller/LibraryWebController.java
+++ src/main/java/com/my_project/my_project/controller/LibraryWebController.java
@@ -1,9 +1,14 @@
 package com.my_project.my_project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.my_project.my_project.model.Book;
 import com.my_project.my_project.service.BookService;
 
 @Controller
@@ -23,5 +28,12 @@ public class LibraryWebController {
 		return "showBooks";
 		
 	}
+
+	// Crear un libro nuevo (POST)
+    @PostMapping("/books")
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book created = bookService.createBook(book);
+        return ResponseEntity.ok(created);
+    }
     
 }
